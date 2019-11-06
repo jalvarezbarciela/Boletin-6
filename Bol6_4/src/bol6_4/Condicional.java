bebf7cb004b7d4af791f66a5804b90f49acc9ce3
jalvarezbarciela <jalvarezbarciela@nautilus6.danielcastelao.org>
2019-11-06 09:26:18 +0100

    Commit correccion v2

--- Bol6_4/src/bol6_4/Condicional.java
+++ Bol6_4/src/bol6_4/Condicional.java
@@ -5,8 +5,8 @@ package bol6_4;
  */
 public class Condicional {
     Peso persona1=new Peso();
-            Peso persona2=new Peso();
-     public Condicional(String nome1,float peso1,String nome2,float peso2){
+    Peso persona2=new Peso();
+    public Condicional(String nome1,float peso1,String nome2,float peso2){
         persona1=new Peso(nome1,peso1);
         persona2=new Peso(nome2,peso2);
     }
@@ -18,9 +18,13 @@ public class Condicional {
         float c=persona1.getPeso()-persona2.getPeso();
         System.out.println(persona1.getNome()+" pesa mais que "+persona2.getNome()+" por "+c+" kilos");
     }
-    else{
+    else{if(persona2.getPeso()>persona1.getPeso()){
         float c=persona2.getPeso()-persona1.getPeso();
         System.out.println(persona2.getNome()+" pesa mais que "+persona1.getNome()+" por "+c+" kilos");
+    }
+    else{
+        System.out.println("Pesan o mesmo");
+    }
     }
     }    
 }
